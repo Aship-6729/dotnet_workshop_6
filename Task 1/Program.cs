1347daeb6e1ec87060c5504139e900ef26ff5e5d
Aship-6729 <aalamaship98@gmail.com>
2025-12-03 11:13:31 +0545

    Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Rectangle rectangle = new Rectangle();
+            rectangle.Length = 5.0;
+            rectangle.Breadth = 3.0;
+
+            Console.WriteLine(rectangle.ShowDetails);
+
+            Console.WriteLine($"Perimeter of a Rectangle: {rectangle.CalculatePerimeter}");
+            Console.WriteLine($"Area of a Rectangle: {rectangle.GetArea}");
+        }
+    }
+}
